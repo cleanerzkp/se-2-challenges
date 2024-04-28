8c3d99c2383889d0fc1c710a96ca5d4a0b7c7870
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2024-04-28 23:41:06 +0200

    buy

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,36 @@
 pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-  // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+contract Vendor is Ownable {
+    YourToken public yourToken;
 
-  YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100; // Price of tokens per ETH
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  // ToDo: create a payable buyTokens() function:
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
+
+    function buyTokens() public payable {
+        require(msg.value > 0, "Vendor: Send some ETH to buy tokens"); // Ensure some ETH is sent
+
+        uint256 tokensToBuy = msg.value * tokensPerEth; // Calculate tokens to buy
+        require(
+            yourToken.balanceOf(address(this)) >= tokensToBuy,
+            "Vendor: Not enough tokens in the contract" // Ensure enough tokens
+        );
+
+        yourToken.transfer(msg.sender, tokensToBuy); // Transfer tokens to buyer
+
+        emit BuyTokens(msg.sender, msg.value, tokensToBuy); // Emit event
+    }
+}
 
   // ToDo: create a withdraw() function that lets the owner withdraw ETH
 
   // ToDo: create a sellTokens(uint256 _amount) function:
-}
+
